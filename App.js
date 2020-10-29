b58b8fffda804ebe48e995c7cb88fc3b896c81d2
Pedro Barros <pedrobarros.dev@gmail.com>
2020-10-28 22:00:50 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+	const [texto, setTexto] = useState("Meu lindo texto");
+
 	return (
 		<h1>Meu lindo site</h1>
 	);
